cc6ce78de5933a2609e9301631f191806a5ec5cc
Mark Florian <markrian@gmail.com>
2013-12-10 14:04:53 +0000

    Attempt at adding emitter.

---
+++ js/emitter.js
@@ -0,0 +1,39 @@
+define(["particle"], function (Particle) {
+
+    function Emitter(x, y, frequency, number, speed, ctx) {
+        this.x = x;
+        this.y = y;
+        this.frequency = frequency;
+        this.speed = speed;
+        this.pool = [];
+        this.particles = [];
+        this.ctx = ctx;
+        this._initParticles(number);
+    }
+
+    Emitter.prototype.update = function update(timestamp) {
+        var dt = timestamp - (this.timestamp || 0);
+
+        var number = this.particles.length;
+        for (var i = 0; i < number; i++) {
+            this.particles[i].update(timestamp);
+        }
+
+        this.timestamp = timestamp;
+    };
+
+    Emitter.prototype._initParticles = function _initParticles(number) {
+        for (var i = 0; i < number; i++) {
+            this.pool.push(
+                new Particle(
+                    this.ctx,
+                    this.x,
+                    this.y,
+                    this.speed,
+                    0
+                )
+            )
+        }
+    };
+
+});
--- js/main.js
+++ js/main.js
@@ -2,16 +2,19 @@ require([
     "log",
     "canvas",
     "particles",
-    "animation"
-    ], function (log, canvas, Particles, animation) {
+    "animation",
+    "emitter"
+    ], function (log, canvas, Particles, animation, Emitter) {
     log("Ready");
 
-    var particles = new Particles(200, 300, canvas.ctx);
+    var emitter = new Emitter(
+        canvas.ctx.canvas.width / 2,
+        canvas.ctx.canvas.height / 2,
+        20, // frequency
+        20 // particle speed
+    );
 
     animation.add(
-        canvas.clear,
-        particles.draw.bind(particles),
-        particles.update.bind(particles)
     );
 
     animation.start();
