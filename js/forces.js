6957d9b9c9c88ca017d5c2f3b71aba047e3509ee
Mark Florian <markrian@gmail.com>
2018-05-30 16:07:36 +0100

    move forces around

--- js/forces.js
+++ js/forces.js
@@ -9,23 +9,75 @@ export default {
         }
     },
     resolve(particle, dt) {
-        // F = m*a;
-        // a = F / m;
-        // dv / dt = F / m
-        // dv = F * dt / m
-
-        // a.b = |a||b|cos(theta);
-
         for (const force of this._forces) {
-            // Var _r_ = [
-            //     force.x - particle.x,
-            //     force.y - particle.y
-            // ];
-            // var r_squared = _r_[0] * _r_[0] + _r_[1] * _r_[1];
-            // var F = force.strength;
-            // var Fx = F * _r_[0] / r_squared;
-
             force.resolve(particle, dt);
         }
     },
 };
+
+export class ConstantForce {
+    constructor(fx, fy) {
+        this.fx = fx;
+        this.fy = fy;
+    }
+
+    resolve(particle, /* dt */) {
+        particle.vx += this.fx;
+        particle.vy += this.fy;
+    }
+
+    draw() {}
+}
+
+export class RadialForce {
+    constructor(ctx, x, y, strength) {
+        this.ctx = ctx;
+        this.x = x;
+        this.y = y;
+        this.direction = strength >= 0 ? 1 : -1;
+        this.strength = Math.abs(strength);
+
+        this._preDraw();
+    }
+
+    resolve(particle, /* dt */) {
+        const direction = [
+            particle.x - this.x,
+            particle.y - this.y,
+        ];
+        let distanceSquared = direction[0] * direction[0] +
+            direction[1] * direction[1];
+
+        if (this.direction < 0) {
+            distanceSquared = Math.sqrt(distanceSquared);
+        }
+
+        const fx = this.strength * this.direction * direction[0] / distanceSquared;
+        const fy = this.strength * this.direction * direction[1] / distanceSquared;
+        particle.vx += fx;
+        particle.vy += fy;
+    }
+
+    draw() {
+        this.ctx.drawImage(this._canvas, this.x - this.strength, this.y - this.strength);
+    }
+
+    _preDraw() {
+        const c = this._canvas = document.createElement('canvas');
+        c.width = c.height = this.strength * 2;
+        const ctx = c.getContext('2d');
+        const radialGradient = ctx.createRadialGradient(
+            this.strength,
+            this.strength,
+            0,
+            this.strength,
+            this.strength,
+            this.strength
+        );
+        const color = this.direction === 0 ? '0,0,127' : '127,0,0';
+        radialGradient.addColorStop(0, 'rgba(' + color + ',1)');
+        radialGradient.addColorStop(1, 'rgba(' + color + ',0)');
+        ctx.fillStyle = radialGradient;
+        ctx.fillRect(0, 0, this.strength * 2, this.strength * 2);
+    }
+}
