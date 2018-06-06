c781927fe4dcf98df9aa1259346edc397c546749
Mark Florian <markrian@gmail.com>
2018-06-06 23:30:21 +0100

    make random accept args in any order
    Fixes bug with start/end angle when one is bigger than the other, and
    when it's less than the other.

--- js/random.js
+++ js/random.js
@@ -1,9 +1,15 @@
 export function between(a, b) {
+    if (a > b) {
+        [a, b] = [b, a];
+    }
     return Math.floor(
         (a + Math.random() * (b - a + 1))
     );
 }
 
 export function betweenFloat(a, b) {
+    if (a > b) {
+        [a, b] = [b, a];
+    }
     return (a + Math.random() * (b - a));
 }
