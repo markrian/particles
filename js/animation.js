e35808c735c206f7599d20812ac2ca0e41f00021
Mark Florian <markrian@gmail.com>
2018-12-27 18:39:15 +0100

    call removeEventListener correctly

--- js/animation.js
+++ js/animation.js
@@ -1,14 +1,16 @@
+const transitionend = 'transitionend';
+
 export function onTransitionEnd(el, fn) {
-    if (!('ontransitionend' in el)) {
+    if (!('on' + transitionend in el)) {
         fn();
         return;
     }
     const listener = function (event) {
         fn(event);
-        el.removeEventListener(listener);
+        el.removeEventListener(transitionend, listener);
     };
 
-    el.addEventListener('transitionend', listener);
+    el.addEventListener(transitionend, listener);
 }
 
 export const rAF = requestAnimationFrame;
